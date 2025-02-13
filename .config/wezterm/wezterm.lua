7f65fa691f87f2c57c6d27de3408a16e3081f37a
Naoya Otsuka <otknoy@gmail.com>
2025-02-13 21:34:10 +0900

    fix wezterm initial_rows

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Modus Vivendi (Gogh)'
 
 config.initial_cols = 160
-config.initial_rows = 48
+config.initial_rows = 40
 
 config.font = wezterm.font 'UDEV Gothic NF'
 config.font_size = 13
